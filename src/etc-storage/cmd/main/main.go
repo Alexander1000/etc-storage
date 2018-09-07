4e072cf2afd46868fdad58a199738f197a3c736b
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-09-07 19:17:44 +0300

    load config from json config

--- src/etc-storage/cmd/main/main.go
+++ src/etc-storage/cmd/main/main.go
@@ -4,12 +4,29 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+	"flag"
+
+	"etc-storage/config"
 )
 
 func main() {
 	log.Println("Starting application ...")
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", 8099), nil); err != nil {
+	configPath := flag.String("c", "", "config file")
+
+	flag.Parse()
+
+	if len(*configPath) == 0 {
+		log.Fatalf("unknown config file")
+	}
+
+	var err error
+	var cfg *config.Config
+	if cfg, err = config.LoadFromFile(*configPath); err != nil {
+		log.Fatalf("error in load config from file: %v", err)
+	}
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil); err != nil {
 		log.Fatalf("error in start application: %v", err)
 	}
 
